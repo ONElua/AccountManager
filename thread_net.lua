e4eed3959afb7e2b33b908f32824c12b15c44c2d
BaltazaR4 <baltazar.regalado@yahoo.com>
2017-06-21 21:13:26 -0500

    Update thread_net.lua

--- thread_net.lua
+++ thread_net.lua
@@ -1,6 +1,6 @@
 -- Global constants
 APP_VERSION_MAJOR = 0x01 -- major.minor
-APP_VERSION_MINOR = 0x10
+APP_VERSION_MINOR = 0x00
 	
 APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
 
